553ca01e41c48d7d42e729d9a239de5ed92db3ec
sandman <sandman8654@outlook.com>
2017-03-02 10:02:57 -0800

    update payment process

--- routes/users.js
+++ routes/users.js
@@ -28,8 +28,7 @@ db.open(function(err, db) {
 });
 exports.increaseUsagePromocode = function(req, res){
     var id = req.params.id;
-    //var user = req.body;
-    console.log('Increasing promocode: ' + id);
+   
     db.collection(promocodeTableName, function(err, collection) {
     //     console.log(collection);
         var pid = new oID(id);
@@ -37,8 +36,7 @@ exports.increaseUsagePromocode = function(req, res){
             var data={'msg':'error'};
             if (err ) {
                 data={'msg':'Error updating code: ' + err};
-                console.log('Error updating code ' + err);
-                //res.send({'error':'An error has occurred'});
+  
             } else {
                var usage = parseInt(item.usage), temp = {};
                temp['code'] = item.code;
@@ -47,8 +45,7 @@ exports.increaseUsagePromocode = function(req, res){
                     if (err){
 
                     }else{
-                        console.log('' + result + ' document(s) updated');
-                        //res.send(user);
+ 
                         data.msg='success';
                     }
                     res.send(data);
@@ -56,27 +53,47 @@ exports.increaseUsagePromocode = function(req, res){
             }
         });
     });
-}
+};
+exports.regUserPaymentStatus = function(req, res){
+    var id = req.params.id;
+   
+    var nowDate = new Date().toString() ;
+ 
+    db.collection(userTableName, function(err, collection) {
+        var pid = new oID(id);
+        collection.update({'_id': pid}, {$set:{'isPremiumUser':true,'isPayDate':nowDate}}, function(err, result) {
+            var data={'msg':'error'};
+            if (err ) {
+                data={'msg':'Error updating user: ' + err};
+   
+            } else {
+ 
+                data.msg='success';
+            }
+            res.send(data);
+        });
+    });
+
+};
+
 exports.changePassword = function(req,res){
     var user = req.body;
     console.log('Updating user: ' + user.email);
     console.log(JSON.stringify(user));
     var data={'msg':'error'};
- //   console.log(new oID(id));
+
     db.collection(userTableName, function(err, collection) {
         var email = user.email, pwd=user.password;
         collection.update({'email': email}, {$set:{'password':pwd}}, function(err, result) {
             
             if (err ) {
                 data={'msg':'Error updating user: ' + err};
-                console.log('Error updating user: ' + err);
-                //res.send({'error':'An error has occurred'});
+   
             } else {
-                console.log('' + result + ' document(s) updated');
-                //res.send(user);
+   
                 data.msg='success';
             }
-     //       res.send(data);
+     
             if (data.msg=='success'){
                 getUserInfo(res,collection,email,pwd);
             }else{
@@ -85,10 +102,10 @@ exports.changePassword = function(req,res){
         });
         
     });
-}
+};
 exports.findAll = function(req, res) {
     db.collection(userTableName, function(err, collection) {
-   //      console.log(collection);
+ 
         collection.find().toArray(function(err, items) {
             res.send(items);
         });
@@ -96,7 +113,7 @@ exports.findAll = function(req, res) {
 };
 var getPromocodes = function(req, res) {
     db.collection(promocodeTableName, function(err, collection) {
-    //     console.log(collection);
+
         collection.find().toArray(function(err, items) {
             console.log(items);
             return items;
@@ -104,19 +121,29 @@ var getPromocodes = function(req, res) {
     });
     return null;
 };
+var checkPaymentStatus = function(item){
+    var nowDate = new Date(),
+        dateStr = item.isPayDate||"01/01/1900",
+        regDate = new Date(dateStr);
+    var diffDays = nowDate.getDate() - regDate.getDate()    ;
+    console.log(diffDays);
+    console.log(dateStr);
+    item.isPremiumUser = (diffDays>30)?false:true;   
+    return item;
+}
 var getUserInfo = function(res,collection, email,pwd){
     var resItem ={};
     collection.findOne({'email':email}, function(err, item) {
-        console.log(email);
+ 
         var data={'msg':'error','item':''};
         if (err ){
             data.msg="Error confirming user: " + err;
-            console.log('Error confirming user: ' + err);
+   
         }else{
             if(item==null){
                 data.msg="No existing user. please sign up now.";
             }else{
-           //     console.log(item);
+   
                 var spwd = item['password'];
                 if (pwd == spwd){ 
                     data.msg = "success"; 
@@ -128,10 +155,11 @@ var getUserInfo = function(res,collection, email,pwd){
              
         }
         if (data.msg =="success"){
-            // collect promocode data items.
+
             db.collection(promocodeTableName, function(err, collection) {
                 collection.find().toArray(function(err, items) {
                     resItem['promocodes'] = items;
+                    resItem = checkPaymentStatus(resItem);
                     data.item = JSON.stringify(resItem);
                     res.send(data);
                 });
@@ -142,63 +170,28 @@ var getUserInfo = function(res,collection, email,pwd){
         }
        
     });
-}
+};
 exports.confirmUser = function(req,res){
     var user = req.body;
     console.log('Checking user: ' + JSON.stringify(user));
     var email = user.email, pwd= user.password;
     var resItem ={};
     db.collection(userTableName, function(err, collection) {
- //       getUserInfo(res,collection,email,pwd);
-        console.log(email + pwd);
-         collection.findOne({'email':email}, function(err, item) {
-            var data={'msg':'error','item':''};
-            if (err ){
-                data.msg="Error confirming user: " + err;
-                console.log('Error confirming user: ' + err);
-            }else{
-                if(item==null){
-                    data.msg="No existing user. please sign up now.";
-                }else{
-               //     console.log(item);
-                    var spwd = item['password'];
-                    if (pwd == spwd){ 
-                        data.msg = "success"; 
-                        resItem = item;
-                    }
-                    else
-                      data.msg="Your password incorrect. please try agin";  
-                }
-                 
-            };
-            if (data.msg =="success"){
-                // collect promocode data items.
-                db.collection(promocodeTableName, function(err, collection) {
-                    collection.find().toArray(function(err, items) {
-                        resItem['promocodes'] = items;
-                        data.item = JSON.stringify(resItem);
-                        res.send(data);
-                    });
-                });  
-            }else{
-                
-                res.send(data);
-            }
-           
-        });
+        getUserInfo(res,collection,email,pwd);
+   
     });
 };
 exports.addUser = function(req, res) {
     var user = req.body;
-    console.log('Adding user: ' + JSON.stringify(user));
+ 
     db.collection(userTableName, function(err, collection) {
         collection.insert(user, {safe:true}, function(err, result) {
             var data={'msg':'error'};
             if (err) {
                data={'msg':'Error adding user: ' + err};
-               console.log('Error adding user: ' + err);
+ 
             } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
+  
                 data={'msg':'success'};
             }
             res.send(data);
@@ -208,40 +201,36 @@ exports.addUser = function(req, res) {
 exports.updateUser = function(req, res) {
     var id = req.params.id;
     var user = req.body;
-    console.log('Updating user: ' + id);
-    console.log(JSON.stringify(user));
- //   console.log(new oID(id));
+  
     db.collection(userTableName, function(err, collection) {
         var pid = new oID(id);
         collection.update({'_id': pid}, user, {'w':1}, function(err, result) {
             var data={'msg':'error'};
             if (err ) {
                 data={'msg':'Error updating user: ' + err};
-                console.log('Error updating user: ' + err);
-                //res.send({'error':'An error has occurred'});
+ 
             } else {
-                console.log('' + result + ' document(s) updated');
-                //res.send(user);
+   
                 data.msg='success';
             }
             res.send(data);
         });
     });
-}
+};
 
 exports.deleteUser = function(req, res) {
     var id = req.params.id;
-    console.log('Deleting user: ' + id);
+
     db.collection(userTableName, function(err, collection) {
         var pid = new oID(id);
-        console.log(pid);
+ 
         collection.remove({'_id':pid}, {'w':1}, function(err, result) {
             var data={'msg':'error'};
             if (err) {
                 data={'msg':'Error deleting user: ' + err};
-                console.log('An error has occurred - ' + err);
+  
             } else {
-                console.log('' + result + ' document(s) deleted');
+
                data.msg='success';
             }
             res.send(data);
@@ -266,7 +255,7 @@ var populateDB = function() {
     db.collection(userTableName, function(err, collection) {
         collection.insert(users, {safe:true}, function(err, result) {
 		
-	});
+	   });
     });
 
 };
@@ -284,7 +273,7 @@ var populatePromocode = function() {
     db.collection(promocodeTableName, function(err, collection) {
         collection.insert(users, {safe:true}, function(err, result) {
         
-    });
+        });
     });
 
 };
